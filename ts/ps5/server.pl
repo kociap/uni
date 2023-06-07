a6f38022013d48ecc5e37e44f197c8274e3cb370
Piotr Kocia <p.kocia@wp.pl>
2023-06-07 19:50:56 +0200

    [ts][ps5] serve all files instead of routing per file

--- ts/ps5/server.pl
+++ ts/ps5/server.pl
@@ -19,13 +19,8 @@ while (my $connection = $d->accept) {
         }
 
         my $url = $request->uri->path;
-        if ($url eq '/') {
-            $connection->send_file_response('webpage/index.html');
-        } elsif ($url eq '/subpage1') {
-            $connection->send_file_response('webpage/subpage1.html');
-        } elsif ($url eq '/subpage2') {
-            $connection->send_file_response('webpage/subpage2.html');
-        } elsif ($url eq '/header') {
+
+        if ($url eq '/header') {
             # Respond with client headers.
             my $headers = $request->headers_as_string;
             my $response = HTTP::Response->new(200);
@@ -33,7 +28,7 @@ while (my $connection = $d->accept) {
             $response->content($headers);
             $connection->send_response($response);
         } else {
-            $connection->send_error(RC_NOT_FOUND)
+            $connection->send_file_response("./webpage" . $url);
         }
     }
 
