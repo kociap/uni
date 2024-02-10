c85adc9e4740da27c853c6112862ba1f0709fcb8
Piotr Kocia <peter.kocia@gmail.com>
2024-02-10 20:59:27 +0100

    [kkd] fix incorrect entropy in ps1

--- kkd/ps1/main.cpp
+++ kkd/ps1/main.cpp
@@ -43,17 +43,19 @@ struct Counter {
 [[nodiscard]] f32 calculate_conditional_entropy(Counter const& counter)
 {
   f32 entropy = 0.0f;
-  for(i64 i = 0; i64 const occurrence: counter.conditional_occurrences) {
-    i64 const y = i / 256;
-    i += 1;
-    if(occurrence <= 0) {
-      continue;
+  for(i64 x = 0; x < 256; x += 1) {
+    f32 subsum = 0.0f;
+    f32 const x_occurrences = static_cast<f32>(counter.occurrences[x]);
+    for(i64 y = 0; y < 256; y += 1) {
+      f32 const occurences = static_cast<f32>(counter.get_conditional_occurrences(y, x));
+      if(occurences <= 0) {
+        continue;
+      }
+      f32 const pyx =  occurences / x_occurrences;
+      subsum += pyx * anton::math::log2(pyx);
     }
-    f32 const total = static_cast<f32>(counter.total);
-    f32 const pxy = static_cast<f32>(occurrence) / total;
-    f32 const y_occurrences = static_cast<f32>(counter.occurrences[y]);
-    f32 const py = y_occurrences / total;
-    entropy -= pxy * (anton::math::log2(pxy) - anton::math::log2(py));
+    f32 const px = x_occurrences / counter.total;
+    entropy -= px * subsum;
   }
   return entropy;
 }
@@ -64,13 +66,14 @@ int main(int argc, char** argv)
   anton::STDERR_Stream stderr_stream;
   if(argc < 2) {
     stderr_stream.write(anton::format("{} <file>"_sv, argv[0]));
-    return -1;
+    return 1;
   }
 
   anton::String const filename(argv[1]);
   anton::fs::Input_File_Stream file(filename);
   if(file.error()) {
     stderr_stream.write(anton::format("error: could not open file '{}'"_sv, filename));
+    return 1;
   }
 
   constexpr i64 page_size = 4096;
@@ -78,8 +81,6 @@ int main(int argc, char** argv)
   u8* const pagebuf = pagebuf_store.data();
   Counter counter{.occurrences = anton::Array<i64>{256, 0},
                   .conditional_occurrences = anton::Array<i64>{256 * 256, 0}};
-  counter.occurrences[0] += 1;
-  counter.total += 1;
   while(!file.eof()) {
     i64 const read = file.read(pagebuf, page_size);
     counter.total += read;
@@ -95,6 +96,8 @@ int main(int argc, char** argv)
   }
 
   f32 const entropy = calculate_entropy(counter);
+  counter.occurrences[0] += 1;
+  counter.total += 1;
   f32 const conditional_entropy = calculate_conditional_entropy(counter);
   stdout_stream.write(
     anton::format("{},{},{}\n"_sv, entropy, conditional_entropy, entropy - conditional_entropy));
